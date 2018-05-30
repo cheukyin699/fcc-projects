2fbcbe4cf7685aa7d138cb1c2a2400c7d9605bf2
Glitch (hurricane-organization) <none>
2018-05-30 22:27:58 +0000

    🍧✂️ Updated with Glitch

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+function getJson(req) {
+  return {
+    ipaddress: req.get("x-forwarded-for").split(',')[0],
+    language: req.get("Accept-Language"),
+    software: req.get("User-Agent")
+  };
+};
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/whoami", function(req, resp) {
+  resp.send(getJson(req));
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
